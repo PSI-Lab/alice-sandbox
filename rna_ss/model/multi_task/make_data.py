926dbb1ca714ce736f69d90feba58155511ceade
AliceDG <alice@deepgenomics.com>
2019-07-04 23:15:32 -0400

    add training

---
+++ rna_ss/model/multi_task/make_data.py
@@ -0,0 +1,155 @@
+# import gzip
+# import csv
+# import numpy as np
+# import pandas as pd
+# import datacorral as dc
+# from genome_kit import Genome, Interval, GenomeTrackBuilder
+# from dgutils.interval import DisjointIntervalsSequence, IntervalData
+# from config import config
+#
+#
+# def parse_expression_data(expression_file, cell_line):
+#     # parse gene expression file downloaded from protein atlas
+#     # rna_celline.tsv
+#     df = pd.read_csv(expression_file, sep='\t')
+#     df = df[['Gene name', 'Sample', 'Value']].rename(columns={'Gene name': 'gene_name',
+#                                                               'Sample': 'cell_line',
+#                                                               'Value': 'tpm'})
+#     assert cell_line in df['cell_line'].unique()
+#     return df[df['cell_line'] == cell_line].drop(columns=['cell_line'])
+#
+#
+# def split_list_equal_sum(a, k):
+#     assert k > 1
+#     assert k < len(a)
+#     result = [[] for _ in range(k)]
+#     # sort descending
+#     a = sorted(a)[::-1]
+#     # go through items in a, add it to the sublist with smallest sum
+#     for x in a:
+#         current_sum = map(sum, result)
+#         idx_sublist = np.argmin(current_sum)
+#         result[idx_sublist].append(x)
+#     return result
+#
+#
+# TODO chromosome size might not be the best proxy, maybe mRNA size?
+# split chromosomes into K folds
+# chrom_sizes = config['chrom_sizes']
+# interval_fold_file_names = config['interval_folds']
+# fold_chrom_sizes = split_list_equal_sum(map(lambda x: x[1], chrom_sizes), len(interval_fold_file_names))
+# fold_chroms = map(lambda x: map(lambda z: next(y[0] for y in chrom_sizes if y[1] == z), x), fold_chrom_sizes)
+# print "chromosomes for each fold:", fold_chroms
+# print "chromosome sizes for each fold:", fold_chrom_sizes
+#
+#
+# genome = Genome(config['genome_annotation'])
+#
+# reader = csv.DictReader(gzip.open(dc.Client().get_path(config['raw_data'])), delimiter='\t')
+# diseqs = dict()
+# default_val = -1
+#
+# track_ss = GenomeTrackBuilder(config['gtrack'], 'f16')  # single value
+# track_ss.set_default_value(default_val)
+#
+# # set of gene symbols passing min TPM
+# df_exp = parse_expression_data(dc.Client().get_path(config['cell_line_gene_expression']), config['cell_line'])
+# expressed_genes = set(df_exp[df_exp['tpm'] >= config['min_tpm']]['gene_name'].tolist())
+# print("Using {} (out of {}, TPM >= {}) highly expressed genes in HepG2".format(len(expressed_genes), len(df_exp),
+#                                                                                config['min_tpm']))
+#
+# skipped_transcripts = set()
+#
+# for row in reader:
+#
+#     tx_id = row['tx_id']
+#     chrom = row['chr']
+#     strand = row['strand']
+#
+#     if tx_id not in diseqs:
+#         try:
+#             transcript = genome.transcripts[tx_id]
+#
+#             # deal with case where multiple transcripts having the same transcript ID
+#             if transcript.chromosome != chrom or transcript.strand != strand:
+#                 transcript = next(
+#                     t for t in genome.transcripts if t.chromosome == chrom and t.strand == strand and t.id == tx_id)
+#         except (KeyError, StopIteration):
+#             if tx_id not in skipped_transcripts:
+#                 print('Cannot get GK transcript %s' % tx_id)
+#                 skipped_transcripts.add(tx_id)
+#             continue
+#
+#         # check whether this gene is expressed in the cell line
+#         if transcript.gene.name not in expressed_genes:
+#             if tx_id not in skipped_transcripts:
+#                 print("{} {} not well expressed in HepG2, skipping".format(transcript.gene.name, transcript.id))
+#                 skipped_transcripts.add(tx_id)
+#             continue
+#
+#         diseqs[tx_id] = DisjointIntervalsSequence(map(lambda x: x.interval, transcript.exons), genome)
+#
+#     diseq = diseqs[tx_id]
+#
+#     # validate
+#     # skip non training/validation chromosomes
+#     if chrom not in config['chrom_all']:
+#         continue
+#
+#     assert diseq.intervals[0].chromosome == chrom, (row, chrom)
+#     assert diseq.intervals[0].strand == strand, (row, diseq.intervals)
+#
+#     transcript_position = int(row['transcript_position'])
+#     genomic_position = int(row['genomic_position'])
+#     base = row['base']
+#
+#     # skip transcript position that's outside of GK transcript length
+#     if transcript_position < 1 or transcript_position > len(diseq.interval):
+#         print('Skip transcript %s position %d' % (tx_id, transcript_position))
+#         continue
+#
+#     # first try using diseq interval
+#     diseq_itv = diseq.interval.end5.shift(transcript_position - 1).expand(0, 1)
+#     if diseq.dna(diseq_itv) == base:
+#         genom_itv = diseq.lower_interval(diseq_itv)[0]
+#         if genom_itv.as_dna1()[0] != genomic_position:
+#             genom_itv = None
+#             pass
+#
+#     # if the above is unsuccessful, fall back to genome position
+#     # need to do this since some transcript definition in the raw data is inconsistent with GK
+#     # e.g. NM_003742 (GK is shorted of 1bp on 5'end)
+#     genom_itv = Interval.from_dna1(chrom, strand, genomic_position, genomic_position, genome)
+#     assert genome.dna(genom_itv) == base
+#
+#     # set gtrack value, make sure use default_val for missing value
+#     if row['reactivity'] != 'NA':
+#         # clip and normalize reactivity value
+#         val = float(row['reactivity'])
+#         if config['reactivity_clip']:
+#             if val <= config['reactivity_min']:
+#                 val = 0
+#             elif val >= config['reactivity_max']:
+#                 val = 1.0
+#             else:
+#                 val = (val - config['reactivity_min'])/float(config['reactivity_max'] - config['reactivity_min'])
+#         assert 0 <= val <= 1
+#     else:
+#         val = default_val
+#
+#     # report if setting overlapping data
+#     try:
+#         track_ss.set_data(genom_itv, np.asarray([val], dtype=np.float16))
+#     except ValueError as e:
+#         print(str(e))
+#         continue
+#
+#
+# track_ss.finalize()
+#
+# # output disjoint intervals for each of the K folds
+# for chroms, file_name in zip(fold_chroms, interval_fold_file_names):
+#     fold_intervals = [x.intervals for x in diseqs.values() if x.intervals[0].chromosome in chroms]
+#     with open(file_name, 'w') as f:
+#         for itv in fold_intervals:
+#             f.write(str(itv) + '\n')
---
+++ rna_ss/model/multi_task/train.py
@@ -0,0 +1,157 @@
+import os
+import sys
+import shutil
+import numpy as np
+from time import gmtime, strftime
+import tensorflow as tf
+import keras
+import datacorral as dc
+import pandas as pd
+from scipy.stats import pearsonr
+from keras import objectives
+import keras.backend as kb
+from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger, Callback
+from genome_kit import Interval
+from data_generator import DataGenerator
+from model import build_model, resolve_contex, custom_loss
+from config import config
+
+
+def _load_interval(file):
+    transcript_itvs = []
+
+    with open(file, 'r') as f:
+        for line in f:
+            line = line.rstrip()
+            transcript_itvs.append(eval(line))
+    return transcript_itvs
+
+
+class Histories(Callback):
+
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.accuracies = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.accuracies.append(logs.get('acc'))
+
+
+class ValidationSetMetrics(Callback):
+
+    def __init__(self, val_data, batch_size=20):
+        super(ValidationSetMetrics, self).__init__()
+        self.validation_data = val_data
+        self.batch_size = batch_size
+
+    def on_train_begin(self, logs={}):
+        self.corr = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        corr = []
+        # get self.batch_size batch of validation data
+        idx_start = np.random.randint(0, len(self.validation_data) - self.batch_size)
+
+        for batch_idx in range(idx_start, idx_start + self.batch_size):
+            _x, _y = self.validation_data[batch_idx]
+            _yp = self.model.predict(_x)
+
+            # compute correlation for each example
+            for k in range(_y.shape[0]):
+                y = _y[k, :]
+                yp = _yp[k, :]
+                # remove missing val
+                idx_valid = np.where(y != -1)  # TODO compute for each dataset
+                y = y[idx_valid]
+                yp = yp[idx_valid]
+                # compute correlation
+                c, p = pearsonr(y, yp)
+                corr.append(c)
+        _df = pd.Series(corr)
+        print("Correlation")
+        print(_df.describe())
+        self.corr.append(_df.median())
+        return
+
+
+def main(validation_fold_idx):
+    chrom_folds = config['chrom_folds']
+    all_intervals = _load_interval(config['all_inervals'])
+    interval_folds = [[] for _ in range(len(chrom_folds))]
+    for itvs in all_intervals:
+        itv_chrom = itvs[0].chromosome
+        for fold_id, fold_chroms in enumerate(chrom_folds):
+            if itv_chrom in fold_chroms:
+                interval_folds[fold_id].append(itvs)
+                break
+    print([len(x) for x in interval_folds])
+
+    training_intervals = [item for idx, sublist in enumerate(interval_folds) for item in sublist if
+                          idx != validation_fold_idx]
+    validation_intervals = interval_folds[validation_fold_idx]
+    print("Validation fold index: %d" % validation_fold_idx)
+    print("Num disjoint intervals in training: %d" % len(training_intervals))
+    print("Num disjoint intervals in validation: %d" % len(validation_intervals))
+
+    training_dataset = DataGenerator(training_intervals)
+    validation_dataset = DataGenerator(validation_intervals)
+
+    sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+    kb.tensorflow_backend._get_available_gpus()
+
+    model = build_model(config['n_filters'], config['residual_conv'], config['n_repeat_in_residual_unit'],
+                        config['skip_conn_every_n'], config['residual'], config['skipconn'], config['gated'])
+    opt = keras.optimizers.Adam(lr=config['learning_rate'], beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0,
+                                amsgrad=False)
+    model.compile(loss=custom_loss,
+                  optimizer=opt)
+
+    # callbacks
+    tictoc = strftime("%Y_%m_%d_%H_%M_%S", gmtime())
+    log_dir = 'log_' + tictoc
+    os.mkdir(log_dir)
+    tensorboard = TensorBoard(log_dir=log_dir)
+
+    run_dir = 'run_' + tictoc
+    os.mkdir(run_dir)
+
+    es_patience = config['es_patience']
+    early_stopping_monitor = EarlyStopping(patience=es_patience, verbose=1)
+
+    callbacks = [
+        Histories(),
+        # ValidationSetMetrics(validation_dataset),  #  TODO add back
+        tensorboard,
+        # FIXME (shreshth)
+        # This should have lower patience than the early stopping monitor right? Otherwise, early-stopping would kick in before LR is reduced. Also makes sense to set a min_lr at 1e-6 or something.
+        ReduceLROnPlateau(patience=10, cooldown=2, verbose=1),
+        early_stopping_monitor,
+        ModelCheckpoint(os.path.join(run_dir, 'checkpoint.{epoch:03d}.hdf5'),
+                        save_best_only=False, period=1),
+        CSVLogger(os.path.join(run_dir, 'history.csv')),
+        Histories(),
+    ]
+
+    model.fit_generator(generator=training_dataset,
+                        validation_data=validation_dataset,
+                        validation_steps=config['num_batch_for_validation'],
+                        callbacks=callbacks,
+                        shuffle=True,
+                        epochs=config['num_epoch'],
+                        use_multiprocessing=True,
+                        workers=2)
+
+    # copy over the best model
+    es_epoch = early_stopping_monitor.stopped_epoch
+    best_epoch = es_epoch - es_patience
+    model_file_src = os.path.join(run_dir, 'checkpoint.{epoch:03d}.hdf5'.format(epoch=best_epoch))
+    model_file_des = os.path.join(config['model_dir'], 'fold_%d.hdf5' % validation_fold_idx)
+    shutil.copy(model_file_src, model_file_des)
+    print("Model from epoch %d is saved at: %s" % (best_epoch, model_file_des))
+
+
+if __name__ == "__main__":
+    fold_idx = int(sys.argv[1])
+    assert 0 <= fold_idx < len(config['chrom_folds'])
+    main(fold_idx)
