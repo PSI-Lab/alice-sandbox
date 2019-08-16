a0ec7f8b5335b72c7a3ffa5f59215ddf76408db9
AliceDG <alice@deepgenomics.com>
2019-08-16 15:10:13 -0400

    code cleanup

---
+++ rna_ss/data_processing/rnafold_mini_data/README.md
@@ -0,0 +1,15 @@
+
+
+Generate toy dataset, for the task:
+
+Given fixed (odd) length sequence, predict the paired probability between each base and the middle base.
+
+Input: sequence of length L
+Output: array of length L
+
+e.g.
+
+```bash
+python make_data.py --len 51 --num 100000
+```
+
--- rna_ss/data_processing/rnafold_mini_data/make_data.py
+++ rna_ss/data_processing/rnafold_mini_data/make_data.py
@@ -1,23 +1,34 @@
 # generate short rand sequence
 # toy dataset
+import argparse
 import random
 import pandas as pd
 from utils import get_pair_prob_arr
 from dgutils.pandas import Column, get_metadata, write_dataframe, add_column
 
 
-seq_len = 51
-num_seqs = 100000
-seqs = []
-for _ in range(num_seqs):
-    seq = ''.join(random.choice(list('ACGU')) for _ in range(seq_len))
-    seqs.append(seq)
+def main(seq_len=51, num_seqs=100000):
+    # checks
+    assert seq_len % 2 == 1, "Sequence length should be odd integer"
 
-df = pd.DataFrame({'sequence': seqs})
-# add mid point pair arr
-df = add_column(df, 'mid_point_pair_prob', ['sequence'], get_pair_prob_arr)
+    seqs = []  # TODO use set so we don't add duplicates (but the probability of getting duplicate is very low)
+    for _ in range(num_seqs):
+        seq = ''.join(random.choice(list('ACGU')) for _ in range(seq_len))
+        seqs.append(seq)
 
-metadata = get_metadata(
-    Column("mid_point_pair_prob", "LIST"))
-write_dataframe(metadata, df, 'data/rand_seqs_{}.csv'.format(num_seqs))
+    df = pd.DataFrame({'sequence': seqs})
+    # add mid point pair arr
+    df = add_column(df, 'mid_point_pair_prob', ['sequence'], get_pair_prob_arr)
+
+    metadata = get_metadata(
+        Column("mid_point_pair_prob", "LIST"))
+    write_dataframe(metadata, df, 'data/rand_seqs_{}.csv'.format(num_seqs))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--len', type=int, help='sequence length')
+    parser.add_argument('--num', type=int, help='total number of sequences to generate')
+    args = parser.parse_args()
+    main(args.len, args.num)
 
