82f11d3f7314001ddcdc45ac693febdb6aa80a11
AliceDG <alice@deepgenomics.com>
2019-08-21 15:07:47 -0400

    toy data 2d

--- rna_ss/data_processing/rnafold_mini_data/make_data_2d.py
+++ rna_ss/data_processing/rnafold_mini_data/make_data_2d.py
@@ -2,15 +2,13 @@
 # toy dataset
 import argparse
 import random
+import cPickle as pickle
 import pandas as pd
-from utils import get_pair_prob_arr
+from utils import get_pair_prob_matrix
 from dgutils.pandas import Column, get_metadata, write_dataframe, add_column
 
 
-def main(seq_len=51, num_seqs=100000):
-    # checks
-    assert seq_len % 2 == 1, "Sequence length should be odd integer"
-
+def main(seq_len=50, num_seqs=100000):
     seqs = []  # TODO use set so we don't add duplicates (but the probability of getting duplicate is very low)
     for _ in range(num_seqs):
         seq = ''.join(random.choice(list('ACGU')) for _ in range(seq_len))
@@ -18,17 +16,14 @@ def main(seq_len=51, num_seqs=100000):
     print("Running rnafold...")
     df = pd.DataFrame({'sequence': seqs})
     # add mid point pair arr
-    df = add_column(df, 'mid_point_pair_prob', ['sequence'], get_pair_prob_arr)
-
-    metadata = get_metadata(
-        Column("mid_point_pair_prob", "LIST"))
-    write_dataframe(metadata, df, 'data/rand_seqs_{}.csv'.format(num_seqs))
+    df = add_column(df, 'pair_matrix', ['sequence'], get_pair_prob_matrix)
+    df.to_pickle('data/rand_seqs_2d_{}.csv.gz'.format(num_seqs), compression='gzip')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--len', type=int, help='sequence length')
-    parser.add_argument('--num', type=int, help='total number of sequences to generate')
+    parser.add_argument('--len', type=int, default=50, help='sequence length')
+    parser.add_argument('--num', type=int, default=100000, help='total number of sequences to generate')
     args = parser.parse_args()
     main(args.len, args.num)
 
--- rna_ss/data_processing/rnafold_mini_data/utils.py
+++ rna_ss/data_processing/rnafold_mini_data/utils.py
@@ -13,6 +13,33 @@ import multiprocessing as mp
 import numpy as np
 
 
+def get_pair_prob_matrix(seq):
+    # get pair prob matrix
+    # no restriction on length
+    tmp_dir = tempfile.mkdtemp()
+    p = Popen(['RNAfold', '--MEA'],
+              cwd=tmp_dir, stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    stdout, stderr = p.communicate(input=seq)
+    rc = p.returncode
+    if rc != 0:
+        msg = 'RNAplfold returned error code %d\nstdout:\n%s\nstderr:\n%s\n' % (
+            rc, stdout, stderr)
+        raise Exception(msg)
+    out_filename = os.path.join(tmp_dir, 'dot.ps')
+    with open(out_filename) as fp:
+        lines = fp.read().splitlines()
+    vals = np.zeros((len(seq), len(seq)))
+    for line in lines:
+        if not line.endswith('ubox') or 'sqrt' in line:  # skip non data, or data header
+            continue
+        p1, p2, sqrt_prob, _ = line.split(' ')
+        p1 = int(p1) - 1
+        p2 = int(p2) - 1
+        vals[p1, p2] = float(sqrt_prob) ** 2
+        vals[p2, p1] = float(sqrt_prob) ** 2
+    return vals
+
+
 def get_pair_prob_arr(seq):
     # for odd length sequence
     # get array of pair probability for every position with the center position
