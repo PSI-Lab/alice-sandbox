6a0340bdec5069f936a6d69174a3178865608599
AliceDG <alice@deepgenomics.com>
2019-08-31 17:14:03 -0400

    variable input size

--- rna_ss/model/rnafold_mini_data_2d_ar/model.py
+++ rna_ss/model/rnafold_mini_data_2d_ar/model.py
@@ -88,40 +88,29 @@ class TriangularConvolution2D(Convolution2D):
 def build_model():
     L12_P = 0.000005
 
-    input_org = Input(shape=(50, 4), name='input_org')
+    input_org = Input(shape=(None, 4), name='input_org')
     # target from the previous 'time stamp'
-    target_ar = Input(shape=(50, 50, 1), name='target_prev')
-
-    # input_rev = Input(shape=(51, 4), name='input_rev')  # can also use rev comp
-    # reverse_layer = Lambda(lambda x: kb.reverse(x, axes=-2))
-    # input_rev = reverse_layer(input_org)
+    target_ar = Input(shape=(None, None, 1), name='target_prev')
 
     conv_prods = []
-    # num_filters = [64, 64, 64, 64, 64]
-    # kernel_sizes = [7, 3, 3, 5, 9]
-
-    # num_filters = [64, 64, 64]
-    # kernel_sizes = [7, 5, 5]
-    # dilation_sizes = [1, 2, 4]
-
-    # num_filters = [64, 64, 64, 64, 64]
-    # kernel_sizes = [7, 5, 5, 5, 5]
-    # dilation_sizes = [1, 2, 2, 4, 4]
-
     num_filters = [256, 256, 256, 256, 256]
     kernel_sizes = [7, 5, 5, 5, 5]
     dilation_sizes = [1, 2, 2, 4, 4]
 
     conv_or = input_org
-    # conv_rv = input_rev   # TODO does it make sense to reverse it for 2d map?
-    conv_rv = input_org
+
+    def repeat_1(x):
+        xr = kb.tile(kb.expand_dims(x, axis=-2), [1, 1, kb.shape(x)[1], 1])
+        return xr
+
+    def repeat_2(x):
+        xr = kb.tile(kb.expand_dims(x, axis=-3), [1, kb.shape(x)[1], 1, 1])
+        return xr
 
     # stacking single nucleotide
     # -1 is feature dim (d=4), -2 is length
-    x1 = Lambda(lambda x: kb.expand_dims(x, axis=-2))(input_org)
-    x2 = Lambda(lambda x: kb.expand_dims(x, axis=-3))(input_org)
-    x1_2 = Lambda(lambda x: kb.tile(x, [1, 1, 50, 1]))(x1)
-    x2_2 = Lambda(lambda x: kb.tile(x, [1, 50, 1, 1]))(x2)
+    x1_2 = Lambda(repeat_1)(input_org)
+    x2_2 = Lambda(repeat_2)(input_org)
     input_nt_stack = concatenate([x1_2, x2_2], axis=-1)
 
     for num_filter, kernel_size, dilation_size in zip(num_filters, kernel_sizes, dilation_sizes):
@@ -136,23 +125,6 @@ def build_model():
         conv_rv = Conv1D(filters=num_filter, kernel_size=kernel_size, dilation_rate=dilation_size,
                          kernel_regularizer=regularizers.l1_l2(l1=L12_P, l2=L12_P),
                          padding='same', activation=None)(conv_rv)
-        # conv_rv_mid = Cropping1D(25)(conv_rv)
-
-        # # transformation matrix - general
-        # conv_or_transform = Conv1D(filters=64, kernel_size=1, activation=None)(conv_or)
-        # conv_prod = Dot(axes=-1)([conv_or_transform, conv_rv_mid])
-        # conv_prods.append(conv_prod)
-
-        # # replace dot product
-        # # by tiling the rev
-        # # stack on org
-        # # run a mini fully connected net
-        # conv_rd_mid_tiled = Lambda(kb.tile, arguments={'n': (1, 51, 1)})(conv_rv_mid)
-        # conv_fw_rd = Concatenate(axis=-1)([conv_or, conv_rd_mid_tiled])
-        # # TODO hard coded 2 filters
-        # # size=1, fully connected along all features at each position
-        # hid_fw_rd = Conv1D(filters=2, kernel_size=1, activation='tanh')(conv_fw_rd)
-        # conv_prods.append(hid_fw_rd)
 
         # dot product
         conv_prod = Dot(axes=-1)([conv_or, conv_rv])  # 2D map
@@ -186,6 +158,110 @@ def build_model():
     model = Model(input=[input_org, target_ar], output=output)
 
     return model
+
+
+# hard-coded len=50
+# def build_model():
+#     L12_P = 0.000005
+#
+#     input_org = Input(shape=(50, 4), name='input_org')
+#     # target from the previous 'time stamp'
+#     target_ar = Input(shape=(50, 50, 1), name='target_prev')
+#
+#     # input_rev = Input(shape=(51, 4), name='input_rev')  # can also use rev comp
+#     # reverse_layer = Lambda(lambda x: kb.reverse(x, axes=-2))
+#     # input_rev = reverse_layer(input_org)
+#
+#     conv_prods = []
+#     # num_filters = [64, 64, 64, 64, 64]
+#     # kernel_sizes = [7, 3, 3, 5, 9]
+#
+#     # num_filters = [64, 64, 64]
+#     # kernel_sizes = [7, 5, 5]
+#     # dilation_sizes = [1, 2, 4]
+#
+#     # num_filters = [64, 64, 64, 64, 64]
+#     # kernel_sizes = [7, 5, 5, 5, 5]
+#     # dilation_sizes = [1, 2, 2, 4, 4]
+#
+#     num_filters = [256, 256, 256, 256, 256]
+#     kernel_sizes = [7, 5, 5, 5, 5]
+#     dilation_sizes = [1, 2, 2, 4, 4]
+#
+#     conv_or = input_org
+#     # conv_rv = input_rev   # TODO does it make sense to reverse it for 2d map?
+#     conv_rv = input_org
+#
+#     # stacking single nucleotide
+#     # -1 is feature dim (d=4), -2 is length
+#     x1 = Lambda(lambda x: kb.expand_dims(x, axis=-2))(input_org)
+#     x2 = Lambda(lambda x: kb.expand_dims(x, axis=-3))(input_org)
+#     x1_2 = Lambda(lambda x: kb.tile(x, [1, 1, 50, 1]))(x1)
+#     x2_2 = Lambda(lambda x: kb.tile(x, [1, 50, 1, 1]))(x2)
+#     input_nt_stack = concatenate([x1_2, x2_2], axis=-1)
+#
+#     for num_filter, kernel_size, dilation_size in zip(num_filters, kernel_sizes, dilation_sizes):
+#         conv_or = BatchNormalization()(conv_or)
+#         conv_or = Activation('relu')(conv_or)
+#         conv_or = Conv1D(filters=num_filter, kernel_size=kernel_size, dilation_rate=dilation_size,
+#                          kernel_regularizer=regularizers.l1_l2(l1=L12_P, l2=L12_P),
+#                          padding='same', activation=None)(conv_or)
+#
+#         conv_rv = BatchNormalization()(conv_rv)
+#         conv_rv = Activation('relu')(conv_rv)
+#         conv_rv = Conv1D(filters=num_filter, kernel_size=kernel_size, dilation_rate=dilation_size,
+#                          kernel_regularizer=regularizers.l1_l2(l1=L12_P, l2=L12_P),
+#                          padding='same', activation=None)(conv_rv)
+#         # conv_rv_mid = Cropping1D(25)(conv_rv)
+#
+#         # # transformation matrix - general
+#         # conv_or_transform = Conv1D(filters=64, kernel_size=1, activation=None)(conv_or)
+#         # conv_prod = Dot(axes=-1)([conv_or_transform, conv_rv_mid])
+#         # conv_prods.append(conv_prod)
+#
+#         # # replace dot product
+#         # # by tiling the rev
+#         # # stack on org
+#         # # run a mini fully connected net
+#         # conv_rd_mid_tiled = Lambda(kb.tile, arguments={'n': (1, 51, 1)})(conv_rv_mid)
+#         # conv_fw_rd = Concatenate(axis=-1)([conv_or, conv_rd_mid_tiled])
+#         # # TODO hard coded 2 filters
+#         # # size=1, fully connected along all features at each position
+#         # hid_fw_rd = Conv1D(filters=2, kernel_size=1, activation='tanh')(conv_fw_rd)
+#         # conv_prods.append(hid_fw_rd)
+#
+#         # dot product
+#         conv_prod = Dot(axes=-1)([conv_or, conv_rv])  # 2D map
+#         # select upper triangular part (lower will be all 0's)
+#         upper_tri_layer = Lambda(lambda x: tf.matrix_band_part(x, 0, -1))
+#         conv_prod = upper_tri_layer(conv_prod)
+#         conv_prods.append(conv_prod)
+#
+#     # stack 2D feature maps
+#     stack_layer = Lambda(lambda x: kb.stack(x, axis=-1))
+#     conv_prod_concat = stack_layer(conv_prods)  # we're doing this to merge multiple (?, 50, 50) layers to (?, 50, 50, K)
+#
+#     # add input nt stack
+#     conv_prod_concat = concatenate([conv_prod_concat, input_nt_stack], axis=-1)
+#
+#     # add target label from previous time stamp
+#     hid = Concatenate(axis=-1)([conv_prod_concat, target_ar])
+#
+#     # triangular conv
+#     # 2x2 (5//2 =2)
+#     hid = TriangularConvolution2D(20, (5, 5), padding='same', activation='relu')(hid)
+#     # 4x4 (9//2 = 4)
+#     hid = TriangularConvolution2D(20, (9, 9), padding='same', activation='relu')(hid)
+#     # 8x8 (17 //2 = 8)
+#     hid = TriangularConvolution2D(20, (17, 17),
+#                                   padding='same', activation='relu')(hid)
+#     # output
+#     output = TriangularConvolution2D(1, (17, 17),
+#                                      padding='same', activation='sigmoid')(hid)
+#
+#     model = Model(input=[input_org, target_ar], output=output)
+#
+#     return model
 
 
 # FIXME is this working?
