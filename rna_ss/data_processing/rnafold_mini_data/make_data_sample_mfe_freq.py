89ac6976609ce9e6fac7d3a131137b91b2285e48
AliceDG <alice@deepgenomics.com>
2019-09-03 22:04:26 -0400

    add script to generate rand seq dataset, prefer high mfe freq seqs

---
+++ rna_ss/data_processing/rnafold_mini_data/make_data_sample_mfe_freq.py
@@ -0,0 +1,43 @@
+# generate short rand sequence
+# toy dataset
+import argparse
+import random
+import cPickle as pickle
+import numpy as np
+import pandas as pd
+from utils import get_fe_struct, one_idx
+from dgutils.pandas import Column, get_metadata, write_dataframe, add_columns, add_column
+
+
+def main(minlen=10, maxlen=100, num_seqs=100000):
+    data = []
+    while len(data) < num_seqs:
+        if len(data) % int(num_seqs//100) == 0:
+            print("Generated {} data points".format(len(data)))
+        seq = ''.join(random.choice(list('ACGU')) for _ in range(random.randint(minlen, maxlen)))
+        pair_matrix, free_energy, mfe_frequency, ensemble_diversity = get_fe_struct(seq)
+        # sample if mfe_frequency < 0.2
+        if mfe_frequency < 0.2 and mfe_frequency < np.random.uniform(0, 1):
+            continue  # skip this one
+        data.append({
+            'seq': seq,
+            'len': len(seq),
+            'free_energy': free_energy,
+            'mfe_frequency': mfe_frequency,
+            'ensemble_diversity': ensemble_diversity,
+            'one_idx': one_idx(pair_matrix),
+        })
+
+    df = pd.DataFrame(data)
+
+    df.to_pickle('data/rand_seqs_var_len_sample_mfe_{}_{}_{}.pkl.gz'.format(minlen, maxlen, num_seqs), compression='gzip')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--minlen', type=int, default=10, help='min sequence length')
+    parser.add_argument('--maxlen', type=int, default=100, help='max sequence length')
+    parser.add_argument('--num', type=int, default=100000, help='total number of sequences to generate')
+    args = parser.parse_args()
+    main(args.minlen, args.maxlen, args.num)
+
