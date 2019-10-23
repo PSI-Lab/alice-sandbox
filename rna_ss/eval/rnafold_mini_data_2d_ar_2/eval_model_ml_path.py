385e159ee2cc9e7dd77c6fa4e5768fbd7eba34ef
AliceDG <alice@deepgenomics.com>
2019-10-23 11:26:12 -0400

    add workflow to generate ml path for a given model+dataset

--- rna_ss/eval/rnafold_mini_data_2d_ar_2/eval_model_ml_path.py
+++ rna_ss/eval/rnafold_mini_data_2d_ar_2/eval_model_ml_path.py
@@ -11,7 +11,7 @@ logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
 
-def _process_row(seq, ml_threshold, model):
+def process_row(seq, ml_threshold, model):
     pred, logp, fe = model.predict_one_step_ar(seq=seq, n_sample=1, start_offset=2,
                                                ml_path=True, ml_threshold=ml_threshold)  # hard-coded offset for now
 
@@ -24,30 +24,13 @@ def _process_row(seq, ml_threshold, model):
     return data_pred
 
 
-def process_row(seq, n_sample, model):
-    # TODO compute CHUNK_SIZE based on sequence length
-    CHUNK_SIZE = 20
-    _n = range(0, n_sample, CHUNK_SIZE)
-    _n.append(n_sample)
-    _n = [b - a for a, b in zip(_n[:-1], _n[1:])]  # batch sizes
-    assert sum(_n) == n_sample
-    if n_sample > CHUNK_SIZE:
-        result = []
-        for _l in _n:
-            _r = _process_row(seq, _l, model)
-            result.extend(_r)
-    else:
-        result = _process_row(seq, n_sample, model)
-    return result
-
-
-def main(model_file, dataset_file, n_sample, output):
+def main(model_file, dataset_file, ml_threshold, output):
     model = PredictorSPlitModel(model_file)
     # TODO make sure dataset has unified format
     df = pd.read_pickle(dataset_file)
 
     df = add_column(df, 'pred_idx', ['seq'],
-                    lambda x: process_row(x, n_sample, model), pbar=True)
+                    lambda x: process_row(x, ml_threshold, model), pbar=True)
 
     df.to_pickle(output)
 
@@ -60,4 +43,4 @@ if __name__ == "__main__":
     parser.add_argument('--output', type=str, help='path to output file')
     args = parser.parse_args()
 
-    main(args.model, args.dataset, args.samples, args.output)
+    main(args.model, args.dataset, args.threshold, args.output)
