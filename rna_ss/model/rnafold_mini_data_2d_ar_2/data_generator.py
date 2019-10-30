ba22ad6430fa11d964cdf75bf968374eabda0508
AliceDG <alice@deepgenomics.com>
2019-10-30 14:13:15 -0400

    infinite data generator [wip]

--- rna_ss/model/rnafold_mini_data_2d_ar_2/data_generator.py
+++ rna_ss/model/rnafold_mini_data_2d_ar_2/data_generator.py
@@ -1,6 +1,10 @@
 import numpy as np
 import keras
+import random
 import itertools
+import pandas as pd
+from StringIO import StringIO
+from subprocess import PIPE, Popen
 import gzip
 import datacorral as dc
 from genome_kit import Genome, GenomeTrack, Interval
@@ -173,6 +177,177 @@ class DataGeneratorVarLen(keras.utils.Sequence):
         y = np.asarray(y)
         y2 = np.asarray(y2)
         return [x1, y], [y, y2]
+
+
+def sample_structures(seq, n_samples):
+    p = Popen(['RNAsubopt',  '-p', str(n_samples)], stdin=PIPE,
+              stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    stdout, stderr = p.communicate(input=seq)
+    rc = p.returncode
+    if rc != 0:
+        msg = 'RNAeval returned error code %d\nstdout:\n%s\nstderr:\n%s\n' % (
+            rc, stdout, stderr)
+        raise Exception(msg)
+    # parse output
+    lines = stdout.splitlines()
+    assert len(lines) == n_samples + 1
+    lines = lines[1:]
+    # convert to idx array
+    all_vals = []
+    for s in lines:
+        assert len(s) == len(seq)
+        # convert to ct file (add a fake energy, otherwise b2ct won't run)
+        input_str = '>seq\n{}\n{} (-0.0)'.format(seq, s)
+        p = Popen(['b2ct'], stdin=PIPE,
+                  stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        stdout, stderr = p.communicate(input=input_str)
+        rc = p.returncode
+        if rc != 0:
+            msg = 'b2ct returned error code %d\nstdout:\n%s\nstderr:\n%s\n' % (
+                rc, stdout, stderr)
+            raise Exception(msg)
+        # load data
+        df = pd.read_csv(StringIO(stdout), skiprows=1, header=None,
+                         names=['i1', 'base', 'idx_i', 'i2', 'idx_j', 'i3'],
+                         sep=r"\s*")
+        assert ''.join(df['base'].tolist()) == seq, ''.join(df['base'].tolist())
+        # matrix
+        vals = np.zeros((len(seq), len(seq)))
+        for _, row in df.iterrows():
+            idx_i = row['idx_i']
+            idx_j = row['idx_j'] - 1
+            if idx_j != -1:
+                vals[idx_i, idx_j] = 1
+                vals[idx_j, idx_i] = 1
+        # process into one-index
+        # set lower triangular to 0
+        vals[np.tril_indices(vals.shape[0])] = 0
+        pred_idx = np.where(vals == 1)
+        all_vals.append(pred_idx)
+    return all_vals
+
+
+class FixedLengthDataBatch(object):
+    """
+    A 'batch' of sequences with the same length.
+    Each sequence has multiple structures associated.
+    It also initialize an counter
+    """
+
+    def __init__(self, num_seq, seq_len, num_structures):
+        self.x = self._generate_sequences(num_seq, seq_len)
+        self.y = self._generate_structures(self.x, num_structures)
+        self._idx = num_structures - 1
+
+    def is_valid(self):
+        # check to see if we've counted down to 0
+        if self._idx >= 0:
+            return True
+        elif self._idx == -1:
+            return False
+        else:
+            raise ValueError  # shouldn't be here
+
+    def pop_data(self):
+        assert self.is_valid()
+        self._idx -= 1
+        return self._get_data(self._idx + 1)
+
+    def _get_data(self, idx):
+        structs = [ss[idx] for ss in self.y]
+        return self.x, structs
+
+    def _generate_sequences(self, num_seq, seq_len):
+        seqs = []  # TODO use set so we don't add duplicates (but the probability of getting duplicate is very low)
+        for _ in range(num_seq):
+            seq = ''.join(random.choice(list('ACGU')) for _ in seq_len)
+            seqs.append(seq)
+        return seqs
+
+    def _generate_structures(self, seqs, num_structures):
+        structs = []
+        for seq in seqs:
+            ss = sample_structures(seq, num_structures)
+            structs.append(ss)
+        return structs
+
+
+class DataGeneratorInfinite(keras.utils.Sequence):
+
+    DNA_ENCODING = np.asarray([[0, 0, 0, 0],
+                               [1, 0, 0, 0],
+                               [0, 1, 0, 0],
+                               [0, 0, 1, 0],
+                               [0, 0, 0, 1]])
+
+    complement_mapping = {
+        'A': 'U',
+        'C': 'G',
+        'G': 'C',
+        'U': 'A',
+    }
+
+    def __init__(self, batch_size, num_batches, min_len=20, max_len=200, num_structures=10):
+        self.batch_size = batch_size
+        self.num_batches = num_batches
+        self.min_len = min_len
+        self.max_len = max_len
+        self.num_structures = num_structures
+        self._data = {i: None for i in num_batches}  # make it dict, easy to use
+
+    def __len__(self):
+        """number of batches per each epoch"""
+        return self.num_batches
+
+    def __getitem__(self, index):
+        """Generate one batch of data"""
+        # check if we need to generate new data
+        if not self._data[index]:
+            self._data[index] = FixedLengthDataBatch(np.random.randint(self.min_len, self.max_len), self.num_structures)
+        _x, _y = self._data[index].pop_data()
+        x, y = self._encode_data(_x, _y)
+
+    def on_epoch_end(self):
+        """decide whether to wipe data"""
+        # wolg, check the first one
+        if not self._data[0] or not self._data[0].is_valid():
+            self._data = {i: None for i in self.num_batches}
+
+    def _encode_seq(self, seq):
+        seq = seq.upper().replace('A', '1').replace('C', '2').replace('G', '3').replace('T', '4').replace('U', '4').replace('N', '0')
+        x = np.asarray(map(int, list(seq)))
+        x = self.DNA_ENCODING[x.astype('int8')]
+        return x
+
+    def _make_pair_arr(self, seq, one_idx):
+
+        def _make_arr(seq, one_idx):
+            target = np.zeros((len(seq), len(seq)))
+            target[one_idx] = 1
+            return target
+
+        def _mask(x):
+            assert len(x.shape) == 2
+            assert x.shape[0] == x.shape[1]
+            x[np.tril_indices(x.shape[0])] = -1
+            return x
+
+        pair_matrix = _make_arr(seq, one_idx)
+        pair_matrix = _mask(pair_matrix)
+        return pair_matrix
+
+    def _encode_data(self, x, y):
+        x_data = []
+        y_data = []
+        assert len(x) == len(y)
+        for seq, one_idx in zip(x, y):
+            _x = self._encode_seq(seq)
+            arr = self._make_pair_arr(seq, one_idx)
+            x_data.append(_x)
+            y_data.append(arr)
+        x_data = np.asarray(x_data)
+        y_data = np.asarray(y_data)
+        return [x_data, y_data], y_data
 
 
 # class DataGeneratorFixedLen(keras.utils.Sequence):
