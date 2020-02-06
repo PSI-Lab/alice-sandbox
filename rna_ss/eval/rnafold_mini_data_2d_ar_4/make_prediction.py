8d86a023c38a655a573a19bb5a25856445843eb8
AliceDG <alice@deepgenomics.com>
2020-02-06 17:53:34 -0500

    remove metrics

--- rna_ss/eval/rnafold_mini_data_2d_ar_4/make_prediction.py
+++ rna_ss/eval/rnafold_mini_data_2d_ar_4/make_prediction.py
@@ -25,29 +25,33 @@ def process_row(seq, threshold, model):
     return data_pred
 
 
-def calculate_metric(one_idx, pred_idx, seq_len, eval):
-    target = np.zeros((seq_len, seq_len))
-    pred = np.zeros((seq_len, seq_len))
-    target[one_idx] = 1
-    pred[pred_idx] = 1
-    sensitivity = eval.sensitivity(pred, target)
-    ppv = eval.ppv(pred, target)
-    return sensitivity, ppv, eval.f_measure(sensitivity, ppv)
+# def calculate_metric(one_idx, pred_idx, seq_len, eval):
+#     target = np.zeros((seq_len, seq_len))
+#     pred = np.zeros((seq_len, seq_len))
+#     target[one_idx] = 1
+#     pred[pred_idx] = 1
+#     sensitivity = eval.sensitivity(pred, target)
+#     ppv = eval.ppv(pred, target)
+#     return sensitivity, ppv, eval.f_measure(sensitivity, ppv)
 
 
-def main(model_file, dataset_file, ml_threshold, output):
+def main(model_file, dataset_file, output):
     model = PredictorSPlitModel(model_file)
     # TODO make sure dataset has unified format
     df = pd.read_pickle(dataset_file)
 
-    df = add_column(df, 'pred_idx', ['seq'],
-                    lambda x: process_row(x, ml_threshold, model), pbar=True)
+    # df = add_column(df, 'pred_idx', ['seq'],
+    #                 lambda x: process_row(x, ml_threshold, model), pbar=True)
 
-    # add metric
-    eval = EvalMetric(bypass_pairing_check=True)
-    df = add_columns(df, ['sensitivity', 'ppv', 'f_measure'],
-                     ['one_idx', 'pred_idx', 'len'],
-                     lambda a, b, c: calculate_metric(a, b, c, eval))
+    # do not apply threshold, get real value pred
+    df = add_column(df, 'pred_idx', ['seq'],
+                    lambda x: process_row(x, None, model), pbar=True)
+
+    # # add metric
+    # eval = EvalMetric(bypass_pairing_check=True)
+    # df = add_columns(df, ['sensitivity', 'ppv', 'f_measure'],
+    #                  ['one_idx', 'pred_idx', 'len'],
+    #                  lambda a, b, c: calculate_metric(a, b, c, eval))
 
     df.to_pickle(output)
 
@@ -56,8 +60,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, help='path to model file')
     parser.add_argument('--dataset', type=str, help='path to dataset file')
-    parser.add_argument('--threshold', type=float, default=None, help='threshold to get binary output, default to not using threshold (real value output)')
+    # parser.add_argument('--threshold', type=float, default=None, help='threshold to get binary output, default to not using threshold (real value output)')
     parser.add_argument('--output', type=str, help='path to output file')
     args = parser.parse_args()
 
-    main(args.model, args.dataset, args.threshold, args.output)
+    # main(args.model, args.dataset, args.threshold, args.output)
+    main(args.model, args.dataset, args.output)
