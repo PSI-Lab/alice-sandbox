db90e8b57a2fbb97acf5ae893bf2c02eeffec95b
Alice Gao <alice@deepgenomics.com>
2020-03-01 18:51:50 -0500

    fix array

--- meetings/2020_03_06/yeast_model/train_nn_multi.py
+++ meetings/2020_03_06/yeast_model/train_nn_multi.py
@@ -98,8 +98,8 @@ class MyDataSet(Dataset):
         # else:
         #     self.y = y
         assert y.shape[1] == 2  # 2 outputs
-        self.y_fitness = y[:, 0]
-        self.y_gi = y[:, 1]
+        self.y_fitness = y[:, [0]]
+        self.y_gi = y[:, [1]]
 
     def __getitem__(self, index):
         _x = self.x[index, :]
