164c5d6801af7d4b8a95362b179470db23e85025
AliceDG <alice@deepgenomics.com>
2020-04-01 16:56:04 -0400

    debug oom

--- meetings/2020_04_07/rna_ss/train_nn.py
+++ meetings/2020_04_07/rna_ss/train_nn.py
@@ -385,9 +385,9 @@ def main(path_data, num_filters, num_stacks, n_epoch, batch_size, max_length, ou
             # running_loss_tr.append(loss.detach().cpu().numpy())
 
             running_loss_tr.append(loss.item())
-            # _r, _p = compute_metrics(y, yp, m)
-            # running_auroc_tr.extend(_r)
-            # running_auprc_tr.extend(_p)
+            _r, _p = compute_metrics(y, yp, m)
+            running_auroc_tr.extend(_r)
+            running_auprc_tr.extend(_p)
             logging.info("Epoch {} Training loss: {}".format(epoch, loss))
 
             model.zero_grad()
